dc0800629001c84e796365ac1d52cd625b50aed8
CLoudytwo <giocogan1@gmail.com>
2025-09-12 21:18:43 +0700

    create page dayat2

--- app/dayat/index.tsx
+++ app/dayat/index.tsx
@@ -1,4 +1,9 @@
+import { Text, View } from "react-native";
 
 export default function DayatIndex() {
-    return
+    return (
+    <View>
+        <Text> ini halaman dayat</Text>
+    </View>
+    );
 }
