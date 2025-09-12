458fedd5ae1ddf65fc5e53585cf7ce52064c9645
Syuby Alwi Akbar Attala <syubyattala806@gmail.com>
2025-09-12 21:11:46 +0700

    Create Page Syuby

---
+++ app/syuby-alwi-akbar-attala/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function SyubyIndex() {
+  return (
+    <View>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+};
