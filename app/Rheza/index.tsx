39c8dceb041cc6b018d6cb6c90448b5ed5ef91f4
imZaaa <rhezarifalsya266@gmail.com>
2025-09-12 21:11:52 +0700

    create page Rheza

--- app/Rheza/index.tsx
+++ app/Rheza/index.tsx
@@ -1,6 +1,6 @@
 import { Text, View } from 'react-native';
 
-export default function Rheza() {
+export default function RhezaIndex() {
     return (
     <View>
         <Text>Ini halaman Rheza</Text>
