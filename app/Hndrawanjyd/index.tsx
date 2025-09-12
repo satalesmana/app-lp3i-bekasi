6244152241d63e67c634d8021cc7a735911b890b
hndrawanjyd <hendrawanjyd@gmail.com>
2025-09-12 21:05:04 +0700

    create page HendrawanJyd

---
+++ app/Hndrawanjyd/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function HendrawanJydindex() {
+    return (
+        <View>
+            <Text>Edit app/index.tsx to edit this screen</Text>
+        </View>
+    );
+};
