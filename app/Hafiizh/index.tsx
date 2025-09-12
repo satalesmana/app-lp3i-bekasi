b65abc5139e0e48ebb12c405bad8cd7203c15b92
Hafiizh452 <hafiizh.alfath33@gmail.com>
2025-09-12 22:14:56 +0700

    create page hafiizh

---
+++ app/Hafiizh/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+        <Text>Hallo ini saya hafiizh</Text>
+    </View>
+  );
+}
+
