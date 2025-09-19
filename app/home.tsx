0b8af88957b9c6feb9e804ae4a9bf0774fa0dfa4
imZaaa <rhezarifalsya266@gmail.com>
2025-09-19 20:38:21 +0700

    Merge branch 'master' of github.com:satalesmana/app-lp3i-bekasi into RhezaRH

---
+++ app/home.tsx
@@ -0,0 +1,21 @@
+import { router } from "expo-router";
+import { Button, Text, View } from "react-native";
+
+
+export default function homeScreen() {
+  return (
+    <View>
+        <Text>ini halaman home</Text>
+        <Button
+          onPress={()=> router.push("/sata-lesmana")}
+          title="Learn More"
+          color="#841584"
+        />
+        <Button
+          onPress={()=> router.push("/Rheza")}
+          title="Rheza"
+          color="#841584"
+        />
+    </View>
+  );
+}
