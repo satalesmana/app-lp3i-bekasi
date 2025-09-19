f32eb391a0bf30520543db18e191d3d39de86d2d
Mufraku <zakkymufra40@gmail.com>
2025-09-19 21:13:01 +0700

    login2

--- app/zakky-mufra/_layout.tsx
+++ app/zakky-mufra/_layout.tsx
@@ -2,7 +2,7 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return(
-    <Stack >
+    <Stack>
       <Stack.Screen name="index" options={{ headerShown: false}} />  
       <Stack.Screen name="login" options={{ headerShown: false}} />
     </Stack>
