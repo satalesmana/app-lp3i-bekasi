0791850b3d6bb9966c1c2d0182be5931b6c4bb3a
sata lesmana <lesmanasata@gmail.com>
2025-09-26 19:59:37 +0700

    re fix

--- app/salsabila-nurul/index.tsx
+++ app/salsabila-nurul/index.tsx
@@ -1,9 +1,57 @@
-import { View, Text } from "react-native";
+import { Image, StyleSheet, Text, View, TouchableOpacity} from "react-native";
+import { router } from "expo-router";
 
-export default function SalsabilaIndex() {
+export default function Index() {
   return (
-    <View>
-      <Text>Ini halaman salsabila</Text>
+    <View style={styles.container}>
+      {/* Text Welcome */}
+      <Text style={styles.title}>Welcome to our app</Text>
+
+      {/* Gambar */}
+      <Image
+        source={require("../../assets/images/logo.png")}
+        style={styles.image}
+        resizeMode="contain"
+      />
+
+      {/* Tombol Start */}
+      <TouchableOpacity style={styles.button}
+      onPress={()=>{router.push('/salsabila-nurul/login')}}>
+        <Text style={styles.buttonText}>Start</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    padding: 20,
+  },
+  image: {
+    width: 310,
+    height: 187,
+    marginBottom: 30,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 40,
+    textAlign: "center",
+  },
+  button: {
+    backgroundColor: "#007bff",
+    paddingVertical: 15,
+    paddingHorizontal: 125,
+    borderRadius: 3,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
