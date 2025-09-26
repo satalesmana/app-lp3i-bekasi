436c3ac87ee86cf7a14e06602dff3ee2f972a2ff
ade261-mi <auliasky2@gmail.com>
2025-09-26 19:59:47 +0700

    login page

--- app/home.tsx
+++ app/home.tsx
@@ -11,6 +11,11 @@ export default function homeScreen() {
           title="Learn More"
           color="#841584"
         />
+        <Button
+          onPress={()=> router.push("/ade")}
+          title="ade"
+          color="#841584"
+        />
     </View>
   );
 }
