d8dfd8b6922462a89778454ed4cf127d2aac1b5e
Syuby Alwi Akbar Attala <syubyattala806@gmail.com>
2025-10-17 20:54:46 +0700

    push

--- app/home.tsx
+++ app/home.tsx
@@ -1,22 +1,38 @@
-import { ButtonPrimary, ButtonSecondary } from "@/componets/myButton";
-import { router } from "expo-router";
-import { Button, Text, View } from "react-native";
+import { ButtonPrimary, ButtonSecondary } from '@/componets/myButton';
+import { router } from 'expo-router';
+import { Button, Text, View } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { decrement, increment } from '../store/reducer/counterSlice';
 
-
 export default function homeScreen() {
-  const count = useSelector(state => state.counter.value)
-  const dispatch = useDispatch()
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
 
   return (
-    <View style={{padding: 15}}>
+    <View>
         <Text>ini halaman home</Text>
         <Button
           onPress={()=> router.push("/sata-lesmana")}
           title="Learn More"
           color="#841584"
         />
+        <Button
+          onPress={()=> router.push("/syuby-alwi-akbar-attala")}
+          title="Tugas Syuby"
+          color="#22a2d4ff"
+        />
+
+        <ButtonPrimary
+          label="Increment"
+          onPress={() => dispatch(increment())}
+          />
+
+        <ButtonSecondary
+          label="Decrement"
+          onPress={() => dispatch(decrement())}
+          />
+
+        <Text>{count}</Text>
     </View>
   );
 }
