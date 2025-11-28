9c1b8d06500abace6d41955ba2a1f83783420b37
ade261-mi <auliasky2@gmail.com>
2025-11-28 20:07:57 +0700

    commit

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,8 +3,8 @@ import { createClient, processLock } from '@supabase/supabase-js'
 import { AppState, Platform } from 'react-native'
 import 'react-native-url-polyfill/auto'
 
-const supabaseUrl = 'https://isibdtuopxlgxxkldvhy.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzaWJkdHVvcHhsZ3h4a2xkdmh5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU1MDE0NzMsImV4cCI6MjA3MTA3NzQ3M30.jIgXj4Tx3ivaTPBcKV8VcXgmNaS410zvbbLrMp20sx0'
+const supabaseUrl = 'https://lsxzypckisvbxwgwaxes.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxzeHp5cGNraXN2Ynh3Z3dheGVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM3MjIyNjgsImV4cCI6MjA3OTI5ODI2OH0.h7VauBBTlm0UUGT5vGCIElK54lFySndS7C0Dn88Ozy0'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
