52284b4253fed67eb062516bfd49998d2c3702e7
sata lesmana <lesmanasata@gmail.com>
2025-12-08 20:39:54 +0700

    form add post

--- app/sata-lesmana/(main)/(post)/form.tsx
+++ app/sata-lesmana/(main)/(post)/form.tsx
@@ -1,9 +1,38 @@
-import { Text, View } from "react-native";
+import { ButtonPrimary } from "@/componets/myButton";
+import { router } from "expo-router";
+import { SafeAreaView, StyleSheet, Text, TextInput } from "react-native";
 
 export default function PostFormPage() {
+    const onSave=()=>{
+        router.back()
+    }
+
     return(
-        <View>
-            <Text>post index</Text>
-        </View>
+        <SafeAreaView style={style.container}>
+            <Text>Post Message</Text>
+            <TextInput 
+                style={style.textInput}
+                editable
+                multiline
+                numberOfLines={4}
+                maxLength={40}/>
+            <ButtonPrimary onPress={onSave} label="Save data" />
+        </SafeAreaView>
     )
 }
+
+const style = StyleSheet.create({
+    container:{
+        margin: 10
+    },
+    textInput: {
+        padding: 10,
+        marginTop:15,
+        borderColor: '#000',
+        borderWidth: 1,
+        height: 200,
+        textAlignVertical:'top',
+        borderRadius: 5,
+
+    }
+})
