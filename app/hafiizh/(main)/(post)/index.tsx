12ab1d82b0aa0fe0328ad0c605e24e535a4783da
Hafiizh452 <hafiizh.alfath33@gmail.com>
2025-12-08 20:46:50 +0700

    update

---
+++ app/hafiizh/(main)/(post)/index.tsx
@@ -0,0 +1,23 @@
+import PostCard from '@/componets/module/post/Card';
+import { FloatingButton } from "@/componets/myButton";
+import { router } from "expo-router";
+import { useState } from "react";
+import { FlatList, SafeAreaView } from "react-native";
+export default function PostPage() {
+    const [data, setData] = useState([])
+
+    const onAddNewPost=()=>{
+        router.push('/hafiizh/(main)/(post)/form')
+    }
+    return(
+        <SafeAreaView style={{ flex: 1 }}>
+            <FlatList
+                data={data}
+                keyExtractor={(item) => item.id.toString()}
+                inverted
+                renderItem={({item}) =><PostCard/> }
+                contentContainerStyle={{ paddingVertical: 10 }} />
+            <FloatingButton onPress={onAddNewPost}/>
+        </SafeAreaView>
+    )
+}
