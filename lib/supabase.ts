bb879a268d3e27d55f39238a21cf58ad921dc4af
Syuby Alwi Akbar Attala <syubyattala806@gmail.com>
2025-12-09 20:39:49 +0700

    changes

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,32 +1,32 @@
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { createClient, processLock } from '@supabase/supabase-js'
-import { AppState, Platform } from 'react-native'
-import 'react-native-url-polyfill/auto'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient, processLock } from '@supabase/supabase-js';
+import { AppState, Platform } from 'react-native';
+import 'react-native-url-polyfill/auto';
 
-const supabaseUrl = 'https://pakhxaoxirlkxtwgxdjm.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBha2h4YW94aXJsa3h0d2d4ZGptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI1MjAyNTQsImV4cCI6MjA3ODA5NjI1NH0.ZcnC3Tid1SkkEfT1lAcYn1uIGYnMeVqwfy87_4wyU7w'
+const supabaseUrl = 'https://pakhxaoxirlkxtwgxdjm.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBha2h4YW94aXJsa3h0d2d4ZGptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI1MjAyNTQsImV4cCI6MjA3ODA5NjI1NH0.ZcnC3Tid1SkkEfT1lAcYn1uIGYnMeVqwfy87_4wyU7w';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    ...(Platform.OS !== "web" ? { storage: AsyncStorage } : {}),
+    ...(Platform.OS !== 'web' ? { storage: AsyncStorage } : {}),
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false,
     lock: processLock,
   },
-})
+});
 
 // Tells Supabase Auth to continuously refresh the session automatically
 // if the app is in the foreground. When this is added, you will continue
 // to receive `onAuthStateChange` events with the `TOKEN_REFRESHED` or
 // `SIGNED_OUT` event if the user's session is terminated. This should
 // only be registered once.
-if (Platform.OS !== "web") {
+if (Platform.OS !== 'web') {
   AppState.addEventListener('change', (state) => {
     if (state === 'active') {
-      supabase.auth.startAutoRefresh()
+      supabase.auth.startAutoRefresh();
     } else {
-      supabase.auth.stopAutoRefresh()
+      supabase.auth.stopAutoRefresh();
     }
-  })
+  });
 }
