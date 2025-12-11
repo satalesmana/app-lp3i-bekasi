92c3e5c875911604e7a20d24572564f002b3faf1
Syuby Alwi Akbar Attala <syubyattala806@gmail.com>
2025-12-11 19:42:53 +0700

    register

--- app/sata-lesmana/(main)/(post)/form.tsx
+++ app/sata-lesmana/(main)/(post)/form.tsx
@@ -1,72 +1,66 @@
-import { ImmagePicker } from "@/componets/ImgPicker";
-import { ButtonPrimary } from "@/componets/myButton";
+import { ImmagePicker } from '@/componets/ImgPicker';
+import { ButtonPrimary } from '@/componets/myButton';
 import { Session } from '@supabase/supabase-js';
-import { router } from "expo-router";
-import { useEffect, useState } from "react";
-import { SafeAreaView, StyleSheet, Text, TextInput } from "react-native";
-import { useDispatch, useSelector } from "react-redux";
+import { router } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { SafeAreaView, StyleSheet, Text, TextInput } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 import { supabase } from '../../../../lib/supabase';
-import { addNewPost, setCreatedBy, setPost } from "../../../../store/reducer/newPostSlice";
+import { addNewPost, setCreatedBy, setPost } from '../../../../store/reducer/newPostSlice';
 
 export default function PostFormPage() {
-    const [session, setSession] = useState<Session | null>(null)
-    const dispatch = useDispatch();
-    const formInput = useSelector((state) => state.post.formInput);
-    
-    useEffect(() => {
-        getSession()
-    }, [session])
+  const [session, setSession] = useState<Session | null>(null);
+  const dispatch = useDispatch();
+  const formInput = useSelector((state) => state.post.formInput);
 
-    const onSave=async ()=>{
-        const name = session?.user.email?.split('@') || ['guest']
+  useEffect(() => {
+    getSession();
+  }, [session]);
 
-        dispatch(setCreatedBy({
-            email: session?.user.email,
-            name: name[0]
-        }))
-        await dispatch(addNewPost(formInput as any) as any)
-        dispatch(setPost(null))
-        router.back()
-    }
+  const onSave = async () => {
+    const name = session?.user.email?.split('@') || ['guest'];
 
-    const getSession=()=>{
-        supabase.auth.getSession().then(({ data: { session } }) => {
-          setSession(session)
-        })
-        supabase.auth.onAuthStateChange((_event, session) => {
-          setSession(session)
-        }) 
-    }
+    dispatch(
+      setCreatedBy({
+        email: session?.user.email,
+        name: name[0],
+      })
+    );
+    await dispatch(addNewPost(formInput as any) as any);
+    dispatch(setPost(null));
+    router.back();
+  };
 
-    return(
-        <SafeAreaView style={style.container}>
-            <Text>Post Message</Text>
-            <TextInput 
-                value={formInput.post}
-                onChangeText={(val)=> dispatch(setPost(val))}
-                style={style.textInput}
-                editable
-                multiline
-                numberOfLines={4}
-                maxLength={40}/>
-            <ImmagePicker label="Select Image" />
-            <ButtonPrimary onPress={onSave} label="Save data" />
-        </SafeAreaView>
-    )
+  const getSession = () => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  };
+
+  return (
+    <SafeAreaView style={style.container}>
+      <Text>Post Message</Text>
+      <TextInput value={formInput.post} onChangeText={(val) => dispatch(setPost(val))} style={style.textInput} editable multiline numberOfLines={4} maxLength={40} />
+      <ImmagePicker label="Select Image" />
+      <ButtonPrimary onPress={onSave} label="Save data" />
+    </SafeAreaView>
+  );
 }
 
 const style = StyleSheet.create({
-    container:{
-        margin: 10
-    },
-    textInput: {
-        padding: 10,
-        marginTop:15,
-        borderColor: '#000',
-        borderWidth: 1,
-        height: 200,
-        textAlignVertical:'top',
-        borderRadius: 5,
-
-    }
-})
+  container: {
+    margin: 10,
+  },
+  textInput: {
+    padding: 10,
+    marginTop: 15,
+    borderColor: '#000',
+    borderWidth: 1,
+    height: 200,
+    textAlignVertical: 'top',
+    borderRadius: 5,
+  },
+});
